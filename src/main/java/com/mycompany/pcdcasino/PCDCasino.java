dd7aa43dca2a19ccb648f940bdd9aa1488087871
javis <javis@Asus-Javi>
2024-04-29 11:48:49 +0200

    Falta completar ruleta

--- src/main/java/com/mycompany/pcdcasino/PCDCasino.java
+++ src/main/java/com/mycompany/pcdcasino/PCDCasino.java
@@ -6,6 +6,7 @@ import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Random;
+import java.util.Scanner;
 
 public class PCDCasino {
 
@@ -21,17 +22,17 @@ public class PCDCasino {
 
             int seleccion = 0;
             while (seleccion != 4) {
-                System.out.println("*************************************************");
-                System.out.println("*               ¡¡ Bienvenido al Casino !!      *");
-                System.out.println("*-----------------------------------------------*");
-                System.out.println("*          Seleccione uno de nuestros juegos:   *");
-                System.out.println("*                                               *");
-                System.out.println("*            1. Ruleta de la Fortuna            *");
-                System.out.println("*            2. Tragaperras Western             *");
-                System.out.println("*            3. Consultar saldo                 *");
-                System.out.println("*            4. Salir                           *");
-                System.out.println("*                                               *");
-                System.out.println("*************************************************");
+                System.out.println("**********************************************");
+                System.out.println("*            ¡¡ Bienvenido al Casino !!      *");
+                System.out.println("*--------------------------------------------*");
+                System.out.println("*       Seleccione uno de nuestros juegos:   *");
+                System.out.println("*                                            *");
+                System.out.println("*         1. Ruleta de la Fortuna            *");
+                System.out.println("*         2. Tragaperras Western             *");
+                System.out.println("*         3. Consultar saldo                 *");
+                System.out.println("*         4. Salir                           *");
+                System.out.println("*                                            *");
+                System.out.println("**********************************************");
                 System.out.println("Seleccione una opción: ");
                 seleccion = (int) ois.readObject();
                 switch (seleccion) {
@@ -75,12 +76,62 @@ public class PCDCasino {
     }
 
     private static void jugarRuleta(Persona p) {
+        
+        //Hacer apuesta
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce el numero al que quuiere apostar: ");
+        int numeroPintao = scanner.nextInt();
+        System.out.println("Introduce la cantidad de fichas que quiere apostar a ese numero");
+        int apuesta = scanner.nextInt();
+        int nuevoSaldo = p.getSaldo() - apuesta; // Supongamos que jugar cuesta 10 unidades
+        
+        //Obtener resultado (numero, color y docena)
+        int[] rojos = {1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36};
+        int[] negros = {2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29 , 31, 33, 35};
+        
         Random random = new Random();
         int resultado = random.nextInt(37); // Números del 0 al 36
-        System.out.println("La ruleta ha caído en el número: " + resultado);
+        
+        String color = null;
+        String docena = null;
+        for (int i = 0; i < rojos.length; i++) {
+            if (rojos[i] == resultado) {
+                color = "Rojo";
+                break; 
+            }
+            if (negros[i] == resultado) {
+                color = "Negro";
+                break; 
+            }
+            else{
+                color = "Verde";
+            }
+            if(0<resultado && resultado<13){
+                docena = "Primera docena";
+            }
+            if(12<resultado && resultado<25){
+                docena = "Segunda docena";
+            }
+            if(24<resultado && resultado<37){
+                docena = "Tercera docena";
+            }
+        }
+        
+        System.out.println("La ruleta ha caído en el número: " + resultado + ", " + color + ", " + docena);
 
-        int nuevoSaldo = p.getSaldo() - 10; // Supongamos que jugar cuesta 10 unidades
+        //Calcular el saldo tras la tirada
+        if(numeroPintao == resultado){
+            nuevoSaldo = p.getSaldo() + apuesta*36;
+        }
+        if (color == "Rojo" && )
+        
         p.setSaldo(nuevoSaldo);
+        System.out.println("Saldo actual después de jugar: " + p.getSaldo());
+        
+        if (nuevoSaldo == 0) {
+            System.out.println("¡No tienes suficiente saldo para jugar!");
+            return;
+        }
     }
 
     private static void jugarTragaperras(Persona p, ObjectOutputStream oos) {
