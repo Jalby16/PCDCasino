115d2b7ecb3055df6845069d6c61b408029928a4
dlmja <dlmja@192.168.1.135>
2024-05-01 14:10:58 +0200

    he cambiao cartas del tarot

--- src/main/java/com/mycompany/pcdcasino/HiloCliente.java
+++ src/main/java/com/mycompany/pcdcasino/HiloCliente.java
@@ -15,22 +15,22 @@ import java.util.Scanner;
  *
  * @author dlmja
  */
-public class HiloCliente implements Runnable{
+public class HiloCliente implements Runnable {
+
     private Socket cliente;
 
     public HiloCliente(Socket cliente) {
         this.cliente = cliente;
     }
-    
-    
+
     @Override
     public void run() {
-        try{
+        try {
             ObjectOutputStream oos = new ObjectOutputStream(cliente.getOutputStream());
             ObjectInputStream ois = new ObjectInputStream(cliente.getInputStream());
 
             int seleccion = 0;
-            
+
             while (seleccion != 5) {
                 System.out.println("*************************************************");
                 System.out.println("*               ¡¡ Bienvenido al Casino !!      *");
@@ -79,115 +79,108 @@ public class HiloCliente implements Runnable{
                 }
             }
             cliente.close();
-            
-            
-        }catch (IOException | ClassNotFoundException e) {
+
+        } catch (IOException | ClassNotFoundException e) {
             System.err.println("Error: " + e.getMessage());
             e.printStackTrace();
             System.exit(1);
         }
     }
+
     private static void jugarRuleta(Persona p) {
         //Hacer apuesta
         Scanner scanner = new Scanner(System.in);
         System.out.println("Introduce la cantidad de fichas que quiere apostar a ese numero (si no quiere apostar a un numero ingrese 0)");
         int apuestaNumero = scanner.nextInt();
         System.out.println("Introduce el numero al que quiere apostar: ");
-        int numeroElegido = scanner.nextInt(); 
-        
-           
+        int numeroElegido = scanner.nextInt();
+
         System.out.println("Introduce la cantidad de fichas que quiere apostar a ese color (si no quiere apostar a un numero ingrese 0)");
         int apuestaColor = scanner.nextInt();
         System.out.println("Introduce el color al que quieres apostar:\n1.Rojo\n2.Negro ");
-        int color = scanner.nextInt(); 
-        
-        
+        int color = scanner.nextInt();
+
         System.out.println("Introduce la cantidad de fichas que quiere apostar a esa docena (si no quiere apostar a un numero ingrese 0)");
         int apuestaDocena = scanner.nextInt();
-            System.out.println("Introduce la docena a la que quieres apostar:\n1.Primera docena\n2.Segunda docena\n3.Tercera docena ");
+        System.out.println("Introduce la docena a la que quieres apostar:\n1.Primera docena\n2.Segunda docena\n3.Tercera docena ");
         int docena = scanner.nextInt();
-        
-        
-        
+
         int nuevoSaldo = p.getSaldo() - (apuestaNumero + apuestaColor + apuestaDocena); // Supongamos que jugar cuesta 10 unidades
         p.setSaldo(nuevoSaldo);
-        
+
         //Obtener resultado (numero, color y docena)
         int[] rojos = {1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36};
-        int[] negros = {2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29 , 31, 33, 35};
-        
-        
+        int[] negros = {2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35};
+
         Random random = new Random();
         int numero = random.nextInt(37); // Números del 0 al 36
-        
+
         String colorResultado = null;
         String docenaResultado = null;
-        
+
         for (int i = 0; i < rojos.length; i++) {
             if (rojos[i] == numero) {
                 colorResultado = "Rojo";
-                break; 
+                break;
             }
             if (negros[i] == numero) {
                 colorResultado = "Negro";
-                break; 
-            }
-            else{
+                break;
+            } else {
                 colorResultado = "Verde";
             }
-            if(0<numero && numero<13){
+            if (0 < numero && numero < 13) {
                 docenaResultado = "Primera docena";
             }
-            if(12<numero && numero<25){
+            if (12 < numero && numero < 25) {
                 docenaResultado = "Segunda docena";
             }
-            if(24<numero && numero<37){
+            if (24 < numero && numero < 37) {
                 docenaResultado = "Tercera docena";
             }
         }
         NumeroRule resultado = new NumeroRule(numero, colorResultado, docenaResultado);
         System.out.println("La ruleta ha caído en el número: " + numero + ", " + colorResultado + ", " + docenaResultado);
-        
 
         //Calcular el saldo tras la tirada
         int ganancias = 0;
-        if(numeroElegido==resultado.getNumero()){
-            ganancias += apuestaNumero*36;
+        if (numeroElegido == resultado.getNumero()) {
+            ganancias += apuestaNumero * 36;
         }
-        
-        if(color==1){
-            if("Rojo".equals(resultado.getColor())){
-                ganancias += apuestaColor*2;
+
+        if (color == 1) {
+            if ("Rojo".equals(resultado.getColor())) {
+                ganancias += apuestaColor * 2;
             }
         }
-        if(color==2){
-            if("Negro".equals(resultado.getColor())){
-                ganancias += apuestaColor*2;
+        if (color == 2) {
+            if ("Negro".equals(resultado.getColor())) {
+                ganancias += apuestaColor * 2;
             }
         }
-        
-        if(docena==1){
-            if ("Primera docena".equals(resultado.getDocena())){
-                ganancias += apuestaDocena*3;
+
+        if (docena == 1) {
+            if ("Primera docena".equals(resultado.getDocena())) {
+                ganancias += apuestaDocena * 3;
             }
         }
-        if(docena==2){
-            if("Segunda docena".equals(resultado.getDocena())){
-                ganancias += apuestaDocena*3;
+        if (docena == 2) {
+            if ("Segunda docena".equals(resultado.getDocena())) {
+                ganancias += apuestaDocena * 3;
             }
         }
-        if(docena==3){
-            if("Tercera docena".equals(resultado.getDocena())){
-                ganancias += apuestaDocena*3;
+        if (docena == 3) {
+            if ("Tercera docena".equals(resultado.getDocena())) {
+                ganancias += apuestaDocena * 3;
             }
         }
-        if(ganancias!=0){
+        if (ganancias != 0) {
             System.out.println("ENHORABUENA HAS GANADO " + ganancias + " FICHAS!!");
             nuevoSaldo = p.getSaldo() + ganancias;
             p.setSaldo(nuevoSaldo);
         }
         System.out.println("Saldo actual después de jugar: " + p.getSaldo());
-        
+
         if (nuevoSaldo == 0) {
             System.out.println("¡No tienes suficiente saldo para jugar!");
             return;
@@ -205,24 +198,24 @@ public class HiloCliente implements Runnable{
         // Simular una tirada de tragaperras
         Random random = new Random();
         String resultado = frutas[random.nextInt(frutas.length)] + " | "
-                        + frutas[random.nextInt(frutas.length)] + " | "
-                        + frutas[random.nextInt(frutas.length)];
+                + frutas[random.nextInt(frutas.length)] + " | "
+                + frutas[random.nextInt(frutas.length)];
         System.out.println(resultado);
         int ganancias = 0;
-        if(resultado.charAt(0)+resultado.charAt(1)==resultado.charAt(3)+resultado.charAt(4) && resultado.charAt(3)+resultado.charAt(4)==resultado.charAt(6)+resultado.charAt(7)){
-            if (";(".equals(resultado.charAt(0))){
-                ganancias+=apuesta*10;
-            }else if (";)".equals(resultado.charAt(0))){
-                ganancias+=apuesta*5;
-            }else if (":0".equals(resultado.charAt(0))){
-                ganancias+=apuesta*4;
-            }else if (":D".equals(resultado.charAt(0))){
-                ganancias+=apuesta*3;
-            }else{
-                ganancias+=apuesta*2;
+        if (resultado.charAt(0) + resultado.charAt(1) == resultado.charAt(3) + resultado.charAt(4) && resultado.charAt(3) + resultado.charAt(4) == resultado.charAt(6) + resultado.charAt(7)) {
+            if (";(".equals(resultado.charAt(0))) {
+                ganancias += apuesta * 10;
+            } else if (";)".equals(resultado.charAt(0))) {
+                ganancias += apuesta * 5;
+            } else if (":0".equals(resultado.charAt(0))) {
+                ganancias += apuesta * 4;
+            } else if (":D".equals(resultado.charAt(0))) {
+                ganancias += apuesta * 3;
+            } else {
+                ganancias += apuesta * 2;
             }
         }
-        if(ganancias!=0){
+        if (ganancias != 0) {
             System.out.println("ENHORABUENA HAS GANADO " + ganancias + " FICHAS!!");
             nuevoSaldo = p.getSaldo() + ganancias;
             p.setSaldo(nuevoSaldo);
@@ -233,27 +226,31 @@ public class HiloCliente implements Runnable{
             return;
         }
 
-         // Supongamos que jugar cuesta 5 unidades
+        // Supongamos que jugar cuesta 5 unidades
         p.setSaldo(nuevoSaldo);
 
     }
-    
-    private static void cartasTarot(Persona p){
-        String[] frasesTarot = {"... el próximo verano harás un viaje a un lugar muy lejano.", "... aprobarás a todos tus alumnos.","... ganarás mucho dinero mañana."};
-        String[] preguntasTarot = {"¿Cual es tu deporte favorito?","¿Cual es tu ciudad favorita?","¿Cual es tu profesion?","¿Cual es tu signo del zodiaco?"};
+
+    private static void cartasTarot(Persona p) {
+        String[] frasesTarot = {"... el próximo verano harás un viaje a un lugar muy lejano.", "... aprobarás a todos tus alumnos.", "... ganarás mucho dinero mañana."};
+        String[] preguntasTarot = {"¿Cual es tu deporte favorito?", "¿Cual es tu ciudad favorita?", "¿Cual es tu profesion?", "¿Cual es tu signo del zodiaco?"};
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
         System.out.println("El coste de cada tirada de cartas es de 10 euros.");
-        System.out.println(preguntasTarot[random.nextInt(preguntasTarot.length)]);
-        String signo = scanner.nextLine();
-        System.out.println("Siendo " + signo + ", las cartas dicen que" + frasesTarot[random.nextInt(frasesTarot.length)]);
-        
-        int nuevoSaldo = p.getSaldo() - 10; // Supongamos que jugar cuesta 10 unidades
-        p.setSaldo(nuevoSaldo);
+        if (p.getSaldo() >= 10) {
+            System.out.println(preguntasTarot[random.nextInt(preguntasTarot.length)]);
+            String signo = scanner.nextLine();
+            System.out.println("Siendo " + signo + ", las cartas dicen que" + frasesTarot[random.nextInt(frasesTarot.length)]);
+
+            int nuevoSaldo = p.getSaldo() - 10; // Supongamos que jugar cuesta 10 unidades
+            p.setSaldo(nuevoSaldo);
+        }else{
+            System.out.println("No tienes suficiente saldo para jugar!!");
+        }
+
     }
-    
 
     private static void consultarSaldo(Persona p) throws IOException {
-        System.out.println("Tiene un saldo de "+p.getSaldo()+"$");
+        System.out.println("Tiene un saldo de " + p.getSaldo() + "$");
     }
 }
